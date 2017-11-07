487e9c3ebad863f59c10c5e606680b9f4b2e0a55
up781212 <up781212@myport.ac.uk>
2017-11-07 01:05:27 +0000

    Added Base Classes
    Implemented all base classes needed for the program.
    Pipe.class
    PipeT1-T5.class
    main.class

---
+++ LongPipesOrderForm/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
