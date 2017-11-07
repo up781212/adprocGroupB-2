9c97ee94a59527f14a3539e5cb3d959d9d45386a
up781212 <up781212@myport.ac.uk>
2017-11-07 01:41:43 +0000

    Added Base Classes

--- LongPipesOrderForm/src/main/Pipe.java
+++ LongPipesOrderForm/src/main/Pipe.java
@@ -1,5 +1,51 @@
 package main;
 
-public class Pipe {
+/* 
+Ah, a glorious Pipe! 
+Or, Picasso's implementation of one... Make sure not to reference this class
+directly to make a pipe. PipeFactory will do the work for you.
+*/
+
+public abstract class Pipe {
+    private int grade; //Grade 1-5
+    private boolean chemicalResistance; //Available for all 
+    private double costPerInch; //In pounds
+    
+    /*
+    ALL INPUT UNITS IN INCHES FOR SIMPLICITY. 
+    MAKE SURE CONVERSION HAS BEEN DONE IN BOXFACTORY!
+    */
+    
+    //All stored in inches
+    private double length; 
+    private double outerDiameter; 
+    private double innerDiameter; 
+    
+  
+    public Pipe(int grade, boolean chemicalResistance, double length, double outerDiameter){
+        this.grade = grade;
+        this.chemicalResistance = chemicalResistance;
+        this.length = length;
+        this.outerDiameter = outerDiameter;
+        innerDiameter = outerDiameter * 0.9;
+    }
+    
+    
+    
+    
+    public int getGrade(){
+        return grade;
+    }
+    
+    public boolean getChemicalResistance(){
+        return chemicalResistance;
+    }
+    public void setChemicalResistance(boolean chemicalResistance){
+        this.chemicalResistance = chemicalResistance;
+    }
+    
+    public double getCostPerInch(){
+        return costPerInch;
+    }
     
 }
---
+++ LongPipesOrderForm/src/main/PipeFactory.java
@@ -0,0 +1,11 @@
+package main;
+
+/*
+Welcome to the Pipe Factory!
+Here pipes are made based on the input criteria given 
+(aka check the different features of the pipe, then create the right tier in the return)
+It's not quite as bad as a Soviet factory, but I'm pretty sure there hasn't been any food in weeks...
+*/
+public class PipeFactory {
+    
+}
