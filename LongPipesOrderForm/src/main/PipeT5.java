494d6d3474a3838059156bdf5de7d995d0fbdc02
Daniel Simpson <UP788777@myport.ac.uk>
2017-11-25 08:14:02 +0000

    Update PipeT5.java

--- LongPipesOrderForm/src/main/PipeT5.java
+++ LongPipesOrderForm/src/main/PipeT5.java
@@ -9,4 +9,16 @@ public class PipeT5 extends PipeT4 {
         costPerInch = new double[]{0.75, 0.8, 0.95};//set cost per inch
         this.outerReinforcement = outerReinforcement;
     }
+    public void calculateCost() {
+        double cost = calculateBaseCost();
+        double costIncrease;
+        costIncrease = cost * 0.16; //add 2 colours
+        costIncrease += cost * 0.13; //adds Inner Insulation
+        costIncrease += cost * 0.17; //adds Outer resistance
+        if (getChemicalResistance()) {
+            costIncrease += cost * 0.14;
+        }
+        cost += costIncrease;
+        //Not sure if this works better?
+    }
 }
