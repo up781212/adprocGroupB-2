42d5ec32bc2042200b775aa0fdc4bd455c5bd721
up781212 <up781212@myport.ac.uk>
2017-11-27 13:40:57 +0000

    Updated Comments

---
+++ PipeT4.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+ *
+ * @author 781212
+ *
+ */
+
+public class PipeT4 extends PipeT3 {
+
+    //costPerInch not needed, T3 and 4 share the same cost per inch.
+    public PipeT4(int grade, boolean chemicalResistance, double length, double outerDiameter) {
+        super(grade, chemicalResistance, length, outerDiameter);
+        calculateCostT4();
+    }
+
+    public void calculateCostT4() {
+        costTotal += baseCost * 0.13;//add Inner insulation
+    }
+}
