7a7b9b9a1925b75673486b531923a27ecaacabf7
up781212 <up781212@myport.ac.uk>
2017-12-02 23:17:01 +0000

    Removed console log

--- LongPipesOrderForm/src/main/PipeT5.java
+++ LongPipesOrderForm/src/main/PipeT5.java
@@ -16,7 +16,6 @@ public class PipeT5 extends PipeT4 {
 
     public void calculateCostT5() {
         costTotal += baseCost * 0.17;//add Inner insulation
-        System.out.println("from T5 " + costTotal);
     }
     
         //override to return correct grade.
